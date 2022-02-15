08f68e5f63de51c3b2d7a7aacf630203c9a19d19
Tenzin <tenthinten@gmail.com>
2022-02-15 23:31:57 +0530

    in html form

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import React, {useState} from 'react';
 const App = () => {
   const[paragraphs,setParagraphs] = useState([]);
   const[tag,setTag] = useState('p');
-  const [inputValue,setInputValue] = useState(1);
+  const[inputValue,setInputValue] = useState(1);
+  const[includeHtml,setIncludeHtml] = useState("Yes")
 
-  
   return (
     <div className="App">
       <h1>Hello</h1>
