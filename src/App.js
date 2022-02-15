013ffeae19c13fc2a6f4e2dff9ae4fcfc359e8ca
Tenzin <tenthinten@gmail.com>
2022-02-15 19:25:01 +0530

    add api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+
+class App extends Component {
+  construct(props) {
+    super(props);
+    this.state = {
+      paras: 4,
+      html : true,
+      text: ''
+    }
+  }
+
+  componentWillMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    );
+  }
 }
 
 export default App;
