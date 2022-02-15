3e4c85e01b19e400769b2c985e282b3959c9c629
Tenzin <tenthinten@gmail.com>
2022-02-15 19:44:56 +0530

    usestate to change text size and generate paras

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
 import './App.css';
-import React, {Component} from 'react';
-import axios from 'axios';
+import React, {useState} from 'react';
 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      paras: 4,
-      html : true,
-      text: ''
-    }
-  }
 
-  componentWillMount() {
-    this.getSampleText();
-  }
-
-  getSampleText() {
-    axios.get('http://hipsterjesus.com/api?paras='+this.state.paras+'&html='+this.state.html)
-      .then((response) => {
-        this.setState({text: response.data.text},function() {
-          console.log(this.state);
-        })
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        Hello
-      </div>
-    );
-  }
-}
+const App = () => {
+  const[paragraphs,setParagraphs] = useState([]);
+  const[tag,setTag] = useState('p');
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+};
 
 export default App;
