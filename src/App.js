e93ff469746c73778efbfabacf1e9fc13b8d29fc
Tenzin <tenthinten@gmail.com>
2022-02-15 23:30:48 +0530

    input value

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import React, {useState} from 'react';
 const App = () => {
   const[paragraphs,setParagraphs] = useState([]);
   const[tag,setTag] = useState('p');
+  const [inputValue,setInputValue] = useState(1);
+
+  
   return (
     <div className="App">
       <h1>Hello</h1>
